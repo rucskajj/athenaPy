a73d40a1cc5d9b3cf846933d9aa1097626d6e1ff
Josef Rucska <rucskajj@mcmaster.ca>
2021-09-28 15:47:36 -0400

    updated procedure for creating file path in readTabs2D routine

--- __init__.py
+++ __init__.py
@@ -1,6 +1,3 @@
 from athenaPy.binary import *
-from athenaPy.XYsim import *
-from athenaPy.plots import *
-from athenaPy.usrexpr import *
-from athenaPy.PLANpy import *
+from athenaPy.tab import *
 
